d017284be9f24a0ad37081381fba6d3ed0701340
richterndzh <richterndzh@gmail.com>
2021-11-30 12:37:52 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -33,3 +33,9 @@ const reviewsSlider = new Swiper('.reviews-slider', {
 $(".newsletter").parallax({
   imageScr: "image/newsletter.jpg",
 });
+
+var menuButton = document.querySelector(".menu-button")
+menuButton.addEventListener('click', function () {
+  console.log("Клик по кнопке меню");
+  document.querySelector(".navbar-button").classList.toggle('navbar-button--visible');
+});

84268b42438b61092524d739a24e6ab729526d11
richterndzh <richterndzh@gmail.com>
2021-12-03 11:13:50 +0300

    create: added modal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function () {
+  const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
@@ -30,12 +31,36 @@ const reviewsSlider = new Swiper('.reviews-slider', {
   },
 });
 
-$(".newsletter").parallax({
-  imageScr: "image/newsletter.jpg",
-});
+  $(".newsletter").parallax({
+    imageScr: "image/newsletter.jpg",
+  });
 
-var menuButton = document.querySelector(".menu-button")
-menuButton.addEventListener('click', function () {
-  console.log("Клик по кнопке меню");
-  document.querySelector(".navbar-button").classList.toggle('navbar-button--visible');
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("Клик по кнопке меню");
+    document
+    .querySelector(".navbar-button")
+    .classList.toggle("navbar-button--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closemodalButton = $(".modal__close");
+  modalButton.on("click", openModal); 
+  closemodalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible"); 
+    modalDialog.addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible"); 
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+
 });

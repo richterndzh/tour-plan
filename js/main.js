a4ff7e51b43cc8a360a1c117aa2f634e6df3ea80
richterndzh <richterndzh@gmail.com>
2021-12-10 00:12:30 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -37,7 +37,6 @@ const reviewsSlider = new Swiper('.reviews-slider', {
 
   var menuButton = document.querySelector(".menu-button");
   menuButton.addEventListener("click", function () {
-    console.log("Клик по кнопке меню");
     document
     .querySelector(".navbar-button")
     .classList.toggle("navbar-button--visible");
